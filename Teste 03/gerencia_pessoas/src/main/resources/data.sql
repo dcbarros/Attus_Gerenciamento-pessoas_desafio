208e63ecbb795582312dbdb2b4fcf0d35619b2e5
dcbarros <dcbarros1@gmail.com>
2024-05-05 16:52:10 -0300

    refactor: Modifica dados do sql de mock dos dados.

--- Teste 03/gerencia_pessoas/src/main/resources/data.sql
+++ Teste 03/gerencia_pessoas/src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO person (uuid, name, birthday, is_active, created_at, updated_at)
 VALUES 
     ('a4dc47fc-32e0-4a84-baa6-647cfe014e65', 'João', '1990-05-15', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     ('d7b04e67-1312-4c68-a8ed-36b17052c6b0', 'Maria', '1985-09-20', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    ('f98d81fc-9c3a-4fb4-8bcf-6b2d81c19b94', 'Pedro', '1982-07-10', false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+    ('f98d81fc-9c3a-4fb4-8bcf-6b2d81c19b94', 'Pedro', '1982-07-10', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- Adiciona endereços
 INSERT INTO address (cep, public_place, number, city, state, is_principal, person_id)
